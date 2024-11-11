2805a57ead6a9f8d9a9ce4d49006ef8243d6400b
Jesse Pavel <jpavel@alum.mit.edu>
2024-11-10 20:09:01 -0500

    Use cl.exe to drive link

--- src/rebuild.bat
+++ src/rebuild.bat
@@ -6,11 +6,10 @@ set LUA_LIBDIR=C:\Code\Tools\Lua\LuaJIT\lib
 set LUA_INCDIR=C:\Code\Tools\Lua\LuaJIT\include
 set SDL_LIBDIR=C:\Code\Tools\SDL2\lib
 set SDL_INCDIR=C:\Code\Tools\SDL2\include\SDL2
-set DLLNAME=moonsdl2.dll
 
 :: Clean up
 del /Q *.obj *.dll *.exp *.lib
 
 :: Build
-cl.exe /c /O2 /std:c11 /MD /LD /nologo /Zc:preprocessor /DMSVC_BUILDDLL /DLUAVER=%LUAVER% /I. /I%LUA_INCDIR% /I%SDL_INCDIR% *.c
-link.exe /DLL /MACHINE:X64 /NOLOGO /OPT:NOREF /LIBPATH:%LUA_LIBDIR% /LIBPATH:%SDL_LIBDIR% /OUT:%DLLNAME% *.obj lua51.lib SDL2.lib SDL2_image.lib SDL2_ttf.lib SDL2_mixer.lib
+cl.exe /c /O2 /std:c11 /MD /nologo /Zc:preprocessor /DMSVC_BUILDDLL /DLUAVER=%LUAVER% /I. /I%LUA_INCDIR% /I%SDL_INCDIR% *.c
+cl.exe *.obj SDL2.lib SDL2_image.lib SDL2_ttf.lib SDL2_mixer.lib lua51.lib /Fe:moonsdl2 /nologo /MD /LD /link /MACHINE:X64 /OPT:NOREF /LIBPATH:%LUA_LIBDIR% /LIBPATH:%SDL_LIBDIR%
